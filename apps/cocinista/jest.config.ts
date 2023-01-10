52d640d82ccd326f956c8e6a715c3fc0e5baef95
Aleix Martínez Suau <aleix.suau@bynd.com>
2023-01-10 16:29:50 +0100

    Initial commit

---
+++ apps/cocinista/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'cocinista',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/cocinista',
+};
