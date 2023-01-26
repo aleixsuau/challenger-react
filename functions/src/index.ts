1de08c1a70d844e2bdc3d5383ff7d3594d04380b
Aleix Martínez Suau <aleix.suau@bynd.com>
2023-01-26 09:10:05 +0100

    DDBB: Save user on creation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,28 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const saveUserToDDBB = functions.auth.user().onCreate(async (user) => {
+  const {
+    displayName,
+    email,
+    emailVerified,
+    phoneNumber,
+    photoURL,
+    providerData,
+    uid,
+  } = user;
+  functions.logger.info("User created >>>", displayName, email,
+      emailVerified, phoneNumber, photoURL, providerData, uid);
+
+  return admin.firestore().collection("users").add({
+    displayName,
+    email,
+    emailVerified,
+    phoneNumber,
+    photoURL,
+    providerData,
+    uid,
+  });
+});
