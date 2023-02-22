e6863a17acaa245266126bb6ebdb5741f185b8ad
Aleix Martínez Suau <aleix.suau@bynd.com>
2023-02-22 20:15:45 +0100

    Add sticky header

--- libs/shell/src/lib/ui/Header/Header.tsx
+++ libs/shell/src/lib/ui/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import styles from './Header.module.scss';
 
 export interface HeaderProps {
@@ -5,10 +6,22 @@ export interface HeaderProps {
 }
 
 export function Header({ children }: HeaderProps) {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const isScrolledListener = () => setIsScrolled(window.pageYOffset > 100);
+
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", isScrolledListener);
+    }
+
+    return () => window.removeEventListener("scroll", isScrolledListener);
+  }, []);
+
   return (
     <header
       id="header"
-      className="fixed top-0 z-30 w-full"
+      className={`sticky top-0 z-30 w-full bg-neutral-content duration-300 ease-in ${isScrolled ? "shadow-sm" : ""}`}
       data-testid="header"
     >
       <div className="container mx-auto px-4 flex w-full flex-wrap items-center py-3 gap-4">
